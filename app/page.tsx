a7ff33ef10a728ce35b878c6ac46215f275b2a75
Pevznergo <102965261+Pevznergo@users.noreply.github.com>
2026-01-17 08:01:03 +0300

    12

--- app/page.tsx
+++ app/page.tsx
@@ -1,26 +1,5 @@
-
-import Header from '../components/Header';
-import Hero from '../components/Hero';
-import HowItWorks from '../components/HowItWorks';
-import ComparisonTable from '../components/ComparisonTable';
-import Pricing from '../components/Pricing';
-import FAQ from '../components/FAQ';
-import Footer from '../components/Footer';
-
-import { Suspense } from 'react';
+import { redirect } from 'next/navigation';
 
 export default function Home() {
-  return (
-    <main className="min-h-screen bg-[#F2F2F7] relative">
-      <Header />
-      <Suspense fallback={null}>
-        <Hero />
-      </Suspense>
-      <HowItWorks />
-      <ComparisonTable />
-      <Pricing />
-      <FAQ />
-      <Footer />
-    </main>
-  );
+  redirect('/next');
 }
