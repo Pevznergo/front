7717679d61daaa0cd10a9f9c60a6d1e3449a3f58
Pevznergo <102965261+Pevznergo@users.noreply.github.com>
2026-01-31 09:44:15 +0300

    Update page.tsx

--- app/app/page.tsx
+++ app/app/page.tsx
@@ -381,6 +381,9 @@ export default function ClanPage() {
                             URL:{" "}
                             {typeof window !== "undefined" ? window.location.href : "N/A"}
                         </p>
+                        <p className="mt-2 text-[10px] text-gray-400">
+                            Hash Length: {typeof window !== "undefined" ? window.location.hash.length : 0}
+                        </p>
                         <p>
                             User ID:{" "}
                             {(typeof window !== "undefined" &&
